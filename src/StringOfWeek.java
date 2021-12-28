20ef94f7ce4813175067fd5168de01085f60cebd
Christine <iplamha@yahoo.com.hk>
2021-12-28 15:46:29 +0800

    interaction with user for calculator

--- src/StringOfWeek.java
+++ src/StringOfWeek.java
@@ -5,18 +5,30 @@ public class StringOfWeek {
         String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
         System.out.println(LongestWeek(week));
+        StringOfWeek.PrintBackwardDays(week);
     }
 
     public static String LongestWeek(String[] week) {
         int longerWeek = 0;
-        int index=0;
+        int index = 0;
         for (int i = 0; i < week.length; i++) {
-            if (week[i].length()> longerWeek) {
+            if (week[i].length() > longerWeek) {
                 longerWeek = week[i].length();
-                index=i;
+                index = i;
             }
         }
         return week[index];
+    }
+
+    public static void PrintBackwardDays(String[] week) {
+        String[] reverseWeek = new String[0];
+        for (int i = week.length - 1; i >= 0; i--) {
+            for (int j = 0; j < week.length; j++) {
+                reverseWeek[j] = week[i];
+                System.out.println(Arrays.toString(reverseWeek));
+            }
+        }
+
     }
 
 }
