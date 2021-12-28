39e31b46add9e045182576cf3966787c25c62ce6
Christine <iplamha@yahoo.com.hk>
2021-12-28 12:37:23 +0800

    interaction with user for calculator

---
+++ src/ArrayInitialisation.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class ArrayInitialisation {
+    public static void main(String[] args) {
+        String[] person={"Anil","Christine","Suresh",new String()};
+        System.out.println(Arrays.toString(person));
+    }
+}
